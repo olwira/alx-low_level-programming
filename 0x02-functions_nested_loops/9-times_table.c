e736bf5739d288b7ebe6a3a167f233fd66ea2246
olwira <olwiranoel@gmail.com>
2022-10-19 09:02:55 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for ( num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
@@ -22,7 +22,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod /10) + '0');
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
