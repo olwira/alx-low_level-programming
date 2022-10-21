7be686ff8cc2407cc73deb072eec169ea4de4c1c
olwira <olwiranoel@gmail.com>
2022-10-21 02:48:12 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase
+ * _isupper - checks if character is uppercase
  * @c: the character to be checked
  * Return: 1 if character is uppercase, 0 otherwise
  */
