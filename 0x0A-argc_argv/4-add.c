c68c60c7f34a1fbef34b6bb4bb1e7aa6d434503f
olwira <olwiranoel@gmail.com>
2022-11-08 01:16:24 -0800

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int result = 0, num, i, j, k;
+
+if (argc == 1)
+printf("0\n");
+
+for (i = 0; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+printf("%s\n", "Error");
+return (1);
+}
+}
+
+for (k = 0; k < argc; k++)
+{
+num = atoi(argv[k]);
+result += num;
+}
+printf("%d\n", result);
+return (0);
+}
